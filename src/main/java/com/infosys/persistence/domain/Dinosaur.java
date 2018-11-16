67a7284198448f4dda99eb4ecc22c863fe3ec04f
tahsinUchiha <alton255@yahoo.co.uk>
2018-11-16 12:32:14 +0000

    added UserBranch

--- src/main/java/com/infosys/persistence/domain/Dinosaur.java
+++ src/main/java/com/infosys/persistence/domain/Dinosaur.java
@@ -1,10 +1,15 @@
 package com.infosys.persistence.domain;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 @Entity
 public class Dinosaur {
@@ -16,9 +21,20 @@ public class Dinosaur {
 	private Long dinosaurid;
 	private String dinosaurName;
 	private String dinosaurNumber;
+	private String givenDinoName;
+	@OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+	private List<Users> users;
 
 	public Dinosaur() {
 
+	}
+
+	public List<Users> getUsers() {
+		return users;
+	}
+
+	public void setUsers(List<Users> users) {
+		this.users = users;
 	}
 
 	public Dinosaur(String dinosaurName, String dinosaurNumber) {
@@ -48,6 +64,19 @@ public class Dinosaur {
 
 	public void setDinosaurNumber(String dinosaurNumber) {
 		this.dinosaurNumber = dinosaurNumber;
+	}
+
+	public String getGivenDinoName() {
+		return givenDinoName;
+	}
+
+	public void setGivenDinoName(String givenDinoName) {
+		this.givenDinoName = givenDinoName;
+	}
+
+	public String giveDinosaur() {
+		return "Dinosaur [DinosaurName=" + dinosaurName + ", Users =" + users + "DinosaurNumber =" + dinosaurNumber
+				+ "]";
 	}
 
 }
